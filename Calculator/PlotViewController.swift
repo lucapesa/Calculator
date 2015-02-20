2fe617a9243981024107662dbd8bfa51da50d415
Luca Pesavento <l.pesa91@gmail.com>
2015-02-20 22:15:24 +0100

    Wrapped in a SplitViewController and set compatibility to Universal

---
+++ Calculator/PlotViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PlotViewController.swift
+//  Calculator
+//
+//  Created by Luca Pesavento on 20/02/2015.
+//  Copyright (c) 2015 Luca Pesavento. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PlotViewController: UIViewController {
+    
+}
